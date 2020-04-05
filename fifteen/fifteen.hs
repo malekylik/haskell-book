371c1f660253c5e8f63aacf074ee2834bdd33c1f
malekylik <malekylik@gmail.com>
2020-04-05 21:49:01 +0300

    Chapter 15: Madness

--- fifteen/fifteen.hs
+++ fifteen/fifteen.hs
@@ -14,3 +14,24 @@ module Fifteen where
         Nada <> (Only v2) = Only v2
         (Only v1) <> Nada = Only v1
         Nada <> Nada = Nada
+
+    type Verb = String
+    type Adjective = String
+    type Adverb = String
+    type Noun = String
+    type Exclamation = String
+
+    madlibbinBetter' :: Exclamation
+        -> Adverb
+        -> Noun
+        -> Adjective
+        -> String
+    madlibbinBetter' e adv noun adj = mconcat
+        [e
+        , "! he said "
+        , adv
+        , " as he jumped into his car "
+        , noun
+        , " and drove off with this "
+        , adj
+        , " wife."]
